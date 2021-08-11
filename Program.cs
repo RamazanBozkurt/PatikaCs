cc0800a3dd6b04e9010ce8295d970ed96b94a790
Ramazan BOZKURT <ramazanbozkurt-20@hotmail.com>
2021-08-11 17:49:35 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleProgramlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Ad : ");
+            string name = Console.ReadLine();
+            System.Console.WriteLine("Soyad : ");
+            string surname = Console.ReadLine();
+
+            System.Console.WriteLine("Merhaba " + name + " " + surname);
+        }
+    }
+}
